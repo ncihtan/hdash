fa652696b86fa61aab259615f3da90015e6ac32c
Ethan Cerami <cerami@gmail.com>
2020-12-24 20:55:31 -0500

    Added credential tests

--- hdash/synapse/credentials.py
+++ hdash/synapse/credentials.py
@@ -12,5 +12,11 @@ class SynapseCredentials:
     def __init__(self):
         """Construct Synapse Credentials."""
         self.user = os.getenv("SYNAPSE_USER")
+        if self.user is None:
+            raise EnvironmentError("SYNAPSE_USER is not set.")
+
         self.password = os.getenv("SYNAPSE_PASSWORD")
+        if self.password is None:
+            raise EnvironmentError("SYNAPSE_PASSWORD is not set.")
+
         logging.info("Using Synapse ID:  %s." % self.user)
---
+++ tests/test_synapse_credentials.py
@@ -0,0 +1,32 @@
+import pytest
+from hdash.synapse.credentials import SynapseCredentials
+from pytest import MonkeyPatch
+
+
+def test_valid_env():
+    """Test Valid Environment."""
+    monkey_patch = MonkeyPatch()
+    monkey_patch.setenv("SYNAPSE_USER", "user")
+    monkey_patch.setenv("SYNAPSE_PASSWORD", "password")
+    credentials = SynapseCredentials()
+    assert credentials.user == "user"
+    assert credentials.password == "password"
+
+
+def test_missing_user():
+    """Test Missing User Environment Variable."""
+    monkey_patch = MonkeyPatch()
+    monkey_patch.delenv("SYNAPSE_USER", raising=False)
+
+    with pytest.raises(EnvironmentError):
+        SynapseCredentials()
+
+
+def test_missing_password():
+    """Test Missing Password Environment Variable."""
+    monkey_patch = MonkeyPatch()
+    monkey_patch.setenv("SYNAPSE_USER", "user")
+    monkey_patch.delenv("SYNAPSE_PASSWORD", raising=False)
+
+    with pytest.raises(EnvironmentError):
+        SynapseCredentials()
