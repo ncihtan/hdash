cea71d42e77b486d681cf63f85bd29a0b7185f54
Ethan Cerami <cerami@gmail.com>
2020-12-27 09:38:18 -0500

    Added fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,43 @@
+import pytest
+from hdash.synapse.htan_project import HTANProject, MetaFile
+
+
+@pytest.fixture
+def init_project_list():
+    return _init_project_list()
+
+
+@pytest.fixture
+def init_annotated_project_list():
+    project_list = _init_project_list()
+    project = project_list[2]
+    project.num_meta = 1
+    meta_file = MetaFile()
+    meta_file.id = "syn2105048111"
+    project.meta_list.append(meta_file)
+    return project_list
+
+
+def _init_project_list():
+    project_list = []
+
+    project_list.append(_create_project("syn23448901", "HTAN MSKCC"))
+    project_list.append(_create_project("syn22093319", "HTAN OHSU"))
+    project_list.append(
+        _create_project(
+            "syn21050481",
+            "HTAN Vanderbilt",
+            "Vesteinn",
+            "Vesteinn coordinating with XXX.",
+        )
+    )
+    return project_list
+
+
+def _create_project(id, name, liaison=None, notes=None):
+    project = HTANProject()
+    project.id = id
+    project.name = name
+    project.liaison = liaison
+    project.notes = notes
+    return project
