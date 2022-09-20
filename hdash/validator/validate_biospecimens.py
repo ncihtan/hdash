9dc6bb2359811d16bc18394d020c29d8e80064d2
Ethan Cerami <cerami@gmail.com>
2022-09-20 10:36:54 -0400

    Updated biospecimen and demographic checks to include error messages

--- hdash/validator/validate_biospecimens.py
+++ hdash/validator/validate_biospecimens.py
@@ -10,7 +10,11 @@ class ValidateBiospecimens(ValidationRule):
     def __init__(self, meta_file_map):
         """Construct new Validation Rule."""
         super().__init__("H_BIOSPEC", "At least one Biospecimen file found.")
-        validation_passed = (Categories.BIOSPECIMEN in meta_file_map) or (
-            Categories.SRRS_BIOSPECIMEN in meta_file_map
-        )
-        self.set_status(validation_passed)
+        check1 = Categories.BIOSPECIMEN in meta_file_map
+        check2 = Categories.SRRS_BIOSPECIMEN in meta_file_map
+        final_check = check1 or check2
+        self.set_status(final_check)
+        if final_check is False:
+            error_list = []
+            error_list.append(f"{Categories.BIOSPECIMEN} file was not found.")
+            self.set_error_list(error_list)
--- hdash/validator/validate_demographics.py
+++ hdash/validator/validate_demographics.py
@@ -1,6 +1,7 @@
 """Validation Rule."""
 
 from hdash.validator.validation_rule import ValidationRule
+from hdash.validator.categories import Categories
 
 
 class ValidateDemographics(ValidationRule):
@@ -9,5 +10,9 @@ class ValidateDemographics(ValidationRule):
     def __init__(self, meta_file_map):
         """Construct new Validation Rule."""
         super().__init__("H_DEM", "At least one Demographics file found.")
-        validation_passed = "Demographics" in meta_file_map
+        validation_passed = Categories.DEMOGRAPHICS in meta_file_map
         self.set_status(validation_passed)
+        if validation_passed is False:
+            error_list = []
+            error_list.append(f"{Categories.DEMOGRAPHICS} file was not found.")
+            self.set_error_list(error_list)
