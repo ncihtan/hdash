6eb1392af20388663d83b2c57ec3f5c18e6bfe33
ecerami <cerami@gmail.com>
2023-03-13 13:57:03 -0400

    Fix issue #9

--- hdash/validator/validate_entity_ids.py
+++ hdash/validator/validate_entity_ids.py
@@ -8,20 +8,24 @@ class ValidateEntityIds(ValidationRule):
 
     def __init__(self, meta_file_map):
         """Construct new Validation Rule."""
-        super().__init__("H_ENTITY_ID", "Synapse Entity IDs are Specified.")
+        categories = Categories()
+        super().__init__("H_ENTITY_ID", "Synapse Entity IDs are Specified for Assays.")
         error_list = []
         for key in meta_file_map:
-            df_list = meta_file_map[key]
-            for df in df_list:
-                try:
-                    synapse_ids = df[Categories.ENTITY_ID_COL].to_list()
-                    self.__check_synapse_ids(key, synapse_ids, error_list)
-                except KeyError:
-                    msg = "%s does not have %s column." % (
-                        key,
-                        Categories.ENTITY_ID_COL,
-                    )
-                    error_list.append(msg)
+            # Check for Entity IDs in All Assays.
+            # Exclude biospecimen and clinical metadata from validation checks.
+            if key in categories.all_assays:
+                df_list = meta_file_map[key]
+                for df in df_list:
+                    try:
+                        synapse_ids = df[Categories.ENTITY_ID_COL].to_list()
+                        self.__check_synapse_ids(key, synapse_ids, error_list)
+                    except KeyError:
+                        msg = "%s does not have %s column." % (
+                            key,
+                            Categories.ENTITY_ID_COL,
+                        )
+                        error_list.append(msg)
         self.set_error_list(error_list)
 
     def __check_synapse_ids(self, key, synapse_ids, error_list):
